9046f6052e18d65dfae9b2a6435abcdb3d98c119
Hydral, Jordan Nathan <hydraldev@gmail.com>
2022-06-05 18:00:50 +0000

    Mettre à jour installer_dev.sh

--- installer_dev.sh
+++ installer_dev.sh
@@ -76,7 +76,7 @@ add_config_rc(){
 
 config_supravim_editor() {
 	echo "COUCOU LINSTALLATION"
-	cp "$INSTALL_DIRECTORY/supravim" $HOME/.local/bin/
+	cp "${INSTALL_DIRECTORY}/supravim" $HOME/.local/bin/
     chmod +x $HOME/.local/bin/supravim
 	#ln -s ${INSTALL_DIRECTORY}/supravim $HOME/.local/bin
 }
