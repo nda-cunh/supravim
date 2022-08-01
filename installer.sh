48e1e21e4e4617b37f10738300203ae1e4ed6baa
Hydral, Jordan Nathan <hydraldev@gmail.com>
2022-08-01 20:50:05 +0000

    solve a bug from new installer

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl https://gitlab.com/hydrasho/SupraVim/-/raw/master/install_vim.sh
+curl https://gitlab.com/hydrasho/SupraVim/-/raw/master/install_vim.sh > install_vim.sh
 ./install_vim.sh master
