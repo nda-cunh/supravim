ac7da485f5fae6905747c719bfff27e93c158061
Nathan, Hydral /Jordan <hydraldev@gmail.com>
2023-07-05 02:00:09 +0000

    first test of suprastore

--- install_vim.sh
+++ install_vim.sh
@@ -107,8 +107,14 @@ config_supravim_editor() {
     ln -sf "${INSTALL_DIRECTORY}/bin/clangd" $HOME/.local/bin/
 	ln -sf "${INSTALL_DIRECTORY}/bin/suprabrain" $HOME/.local/bin/
     ln -sf "${INSTALL_DIRECTORY}/bin/supramake" $HOME/.local/bin/
+	ln -sf "${INSTALL_DIRECTORY}/bin/suprastore" $HOME/.local/bin/
+	chmod +x $HOME/.local/bin/clangd
 	chmod +x $HOME/.local/bin/suprabrain
-	chmod +x $HOME/.local/share/SupraVim/bin/suprago
+	chmod +x $HOME/.local/bin/supramake
+	chmod +x $HOME/.local/bin/supratags
+	chmod +x $HOME/.local/bin/supratags
+	chmod +x $HOME/.local/bin/suprastore
+
 	mkdir -p $HOME/.local/share/fonts
     mv "${INSTALL_DIRECTORY}/data/ubuntuNerdFont.ttf" $HOME/.local/share/fonts/ubuntuNerdFont.ttf
 }
