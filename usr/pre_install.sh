38369126e85bfd85eb7d51c003df61bb9452ffb8
Nda-cunh <hydraldev@gmail.com>
2023-10-29 18:47:47 +0000

    Update vim

--- usr/pre_install.sh
+++ usr/pre_install.sh
@@ -1,3 +1,5 @@
 echo "Kill all vim before update"
 # killall vim 2> /dev/null
 echo "get last_config ..."
+value=$($SRCDIR/share/supravim/create_cfg "$SRCDIR/share/supravim/supravim.cfg")
+exit $value
