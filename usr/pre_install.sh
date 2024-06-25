191e7551b0b511f1cf0a58ead06a565c920465d1
Nda-cunh <hydraldev@gmail.com>
2024-06-25 14:32:59 +0200

    Fix bug when first install of supravim

--- usr/pre_install.sh
+++ usr/pre_install.sh
@@ -2,5 +2,5 @@ rm -rf $HOME/.local/share/SupraVim
 echo "Kill all vim before update"
 # killall vim 2> /dev/null
 echo "get last_config ..."
-value=$($SRCDIR/bin/supravim --save-config)
+value=$($SRCDIR/bin/supravim --save-config) 2>/dev/null | true
 exit $!
