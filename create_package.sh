b1c2ec2093eddbc228c13ec077b7c086d2fde58a
Nda-cunh, Nathan <hydraldev@gmail.com>
2025-01-26 02:04:14 +0000

    add supravim-server create package

--- create_package.sh
+++ create_package.sh
@@ -12,11 +12,12 @@ git clone https://gitlab.com/supraproject/supramake supramake
 git clone https://gitlab.com/supraproject/suprabear suprabear
 git clone https://gitlab.com/nda-cunh/supravim-gui supravim-gui
 git clone https://gitlab.com/nda-cunh/makeheader.git makeheader
-
+git clone https://gitlab.com/supraproject/supravim-server.git
 
 create_package supramake
 create_package suprabear
 create_package supravim-gui
 create_package makeheader
+create_package supravim-server
 
 mv */*.suprapack .
