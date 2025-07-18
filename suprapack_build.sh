7a991ad82290f8fb881509f6c557b647105fc329
nda-cunh <hydraldev@gmail.com>
2025-07-19 01:39:53 +0200

    Add a patch for vim-devicons

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+export PATCH_DIR=$MESON_SOURCE_ROOT/patch
 mkdir -p $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle
 pushd $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle/
 ls -la
 git clone https://github.com/noscript/elevator.vim
 git clone https://github.com/ryanoasis/vim-devicons
+pushd vim-devicons
+git apply $PATCH_DIR/more-icons.patch
+popd
 git clone https://github.com/preservim/nerdtree
 git clone https://github.com/Donaldttt/fuzzyy
 git clone https://github.com/jiangmiao/auto-pairs
