e89dfb12df77f949b6f0ee9471a4fb6a1e612608
nda-cunh <hydraldev@gmail.com>
2025-08-23 14:42:44 +0200

    Link directly all github supravim-plugin instead of actualize it in the folder, and remove all mkview/loadview

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -3,15 +3,17 @@
 export PATCH_DIR=$MESON_SOURCE_ROOT/patch
 mkdir -p $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle
 pushd $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle/
-git clone https://github.com/noscript/elevator.vim
-# git clone https://github.com/ryanoasis/vim-devicons
-# pushd vim-devicons
-# git apply $PATCH_DIR/more-icons.patch
-# popd
-# git clone https://github.com/preservim/nerdtree
-git clone https://github.com/Donaldttt/fuzzyy
-git clone https://github.com/jiangmiao/auto-pairs
-git clone https://github.com/vim-airline/vim-airline.git
+
+
+git clone https://github.com/noscript/elevator.vim --depth 1
+git clone https://github.com/Donaldttt/fuzzyy --depth 1
+git clone https://github.com/jiangmiao/auto-pairs --depth 1
+git clone https://github.com/vim-airline/vim-airline --depth 1
+git clone https://github.com/nda-cunh/SupraSnake --depth 1
+git clone https://github.com/nda-cunh/SupraTree --depth 1
+git clone https://github.com/nda-cunh/SupraIcons --depth 1
+git clone https://github.com/nda-cunh/SupraPopup --depth 1
+
 rm -rf */.git
 rm -rf */.github
 rm -rf */README.md
