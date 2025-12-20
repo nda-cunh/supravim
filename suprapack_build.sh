b47973623b89117c18c82d54efaf82938521581e
nda-cunh <hydraldev@gmail.com>
2025-12-20 02:43:00 +0100

    Add SupraAutoPairs

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -4,11 +4,10 @@ export PATCH_DIR=$MESON_SOURCE_ROOT/patch
 mkdir -p $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle
 pushd $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle/
 
-
 git clone https://github.com/noscript/elevator.vim --depth 1
 git clone https://github.com/Donaldttt/fuzzyy --depth 1
-git clone https://github.com/jiangmiao/auto-pairs --depth 1
 git clone https://github.com/vim-airline/vim-airline --depth 1
+git clone https://github.com/nda-cunh/vim9-autopairs.git --depth 1
 git clone https://github.com/nda-cunh/SupraSnake --depth 1
 git clone https://github.com/nda-cunh/SupraTree --depth 1
 git clone https://github.com/nda-cunh/SupraIcons --depth 1
