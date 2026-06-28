4b2c53c164d17f4b9c6d1fffe95da0c9bd6e6f53
nda-cunh <hydraldev@gmail.com>
2026-06-28 07:10:27 +0200

    chore(build): update suprapack build to clone ScrollBar plugin and reorder external plugin clones

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -4,9 +4,8 @@ export PATCH_DIR=$MESON_SOURCE_ROOT/patch
 mkdir -p $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle
 pushd $MESON_INSTALL_DESTDIR_PREFIX/share/supravim/vim/bundle/
 
-git clone https://github.com/noscript/elevator.vim --depth 1
-git clone https://github.com/Donaldttt/fuzzyy --depth 1
-git clone https://github.com/vim-airline/vim-airline --depth 1
+# External SupraVim plugins
+git clone https://github.com/nda-cunh/ScrollBar.git --depth 1
 git clone https://github.com/nda-cunh/vim9-autopairs.git --depth 1
 git clone https://github.com/nda-cunh/SupraSnake --depth 1
 git clone https://github.com/nda-cunh/SupraTree --depth 1
@@ -16,6 +15,9 @@ git clone https://github.com/nda-cunh/SupraIcons --depth 1
 git clone https://github.com/nda-cunh/SupraPopup --depth 1
 git clone https://github.com/nda-cunh/SupraPacman --depth 1
 git clone https://github.com/nda-cunh/PropColor --depth 1
+# External plugins
+git clone https://github.com/Donaldttt/fuzzyy --depth 1
+git clone https://github.com/vim-airline/vim-airline --depth 1
 git clone https://github.com/hrsh7th/vim-vsnip --depth 1
 git clone https://github.com/hrsh7th/vim-vsnip-integ --depth 1
 
@@ -23,4 +25,5 @@ rm -rf */.git
 rm -rf */test
 rm -rf */.github
 rm -rf */*.md
+
 popd
