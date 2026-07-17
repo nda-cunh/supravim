d7200e627b2e9e4b79ba71b7c18e048439f6ab25
nda-cunh <hydraldev@gmail.com>
2026-07-17 04:07:45 +0200

    build(packaging): add dvd.vim dependency to build script

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -16,6 +16,7 @@ git clone https://github.com/nda-cunh/SupraPopup --depth 1
 git clone https://github.com/nda-cunh/SupraPacman --depth 1
 git clone https://github.com/nda-cunh/PropColor --depth 1
 git clone https://github.com/nda-cunh/Crumble.vim --depth 1
+git clone https://github.com/nda-cunh/dvd.vim --depth 1
 # External plugins
 git clone https://github.com/Donaldttt/fuzzyy --depth 1
 git clone https://github.com/vim-airline/vim-airline --depth 1
